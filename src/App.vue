2156cb48a839929bb4de85ad335c035314a2be92
Abhiyan <iabhiyaan@gmail.com>
2022-03-25 14:47:45 +0545

    :zap: initial setup

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script>
+import {defineComponent} from 'vue';
+
+import './VidyoClient'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  },
+  mounted() {
+    console.log(window.VidyoClientLib)
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
